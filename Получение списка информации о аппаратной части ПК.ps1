77fa37542226b9e3367dca4952cbec1e0e9159c7
ArsIS-02 <74455553+ArsIS-02@users.noreply.github.com>
2022-01-04 13:54:14 +0500

    Add files via upload

---
+++ Получение списка информации о аппаратной части ПК.ps1
@@ -0,0 +1,6 @@
+﻿<#Windows PowerShell 5.1
+Script by Arsentyev Igor
+Получение списка информации о аппаратной части ПК
+Формирует файл PnP.html на системном диске, открывать в веб-браузере
+#>
+Get-WmiObject Win32_PnPEntity | Select-Object Name, Manufacturer, Status, PNPDeviceID | ConvertTo-HTML -Head "<link rel='stylesheet' href='c:\styles.css' type='text/css' />" | Out-File c:\PnP.html
